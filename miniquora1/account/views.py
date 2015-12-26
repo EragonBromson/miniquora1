d48748bd7bd8e14e3d4f301b310918d2d5ae6059
Sarthak Agrawal <sarthak.agrawal76@gmail.com>
2015-12-26 22:20:04 +0530

    Corrected urls.py

--- miniquora1/account/views.py
+++ miniquora1/account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators.http import require_http_methods, require_GET, require_POST
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
+
 
 # Create your views here.
 
@@ -9,10 +10,10 @@ from django.contrib.auth.decorators.http import require_http_methods, require_GE
 def base(request):
     return render(request,'base/base.html')
 
-@require_POST
+#@require_POST
 def login(request):
     return HttpResponse('LoginForm to be displayed.')
 
-@login_required
+#@login_required
 def secret(request):
     return HttpResponse('User logged in')
