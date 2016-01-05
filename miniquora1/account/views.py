ea7e4c5ce4816c8a47cff2de33f991912733c079
Sarthak Agrawal <sarthak.agrawal76@gmail.com>
2016-01-06 00:43:09 +0530

    completed base.html but not rendered form

--- miniquora1/account/views.py
+++ miniquora1/account/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
-
+from .forms import LoginForm
 
 # Create your views here.
 
 @require_GET
 def base(request):
+    if(request.user.is_authenticated()):
+        return HttpResponse('User is authenticated')
+    f=LoginForm(initial = { 'username' : 'admin' })
+    #THIS IS AN UNBOUNDED FORM WHICH WILL BE RENDERED ON BASE
+    context = { 'form' : f }
     return render(request,'base/base.html')
 
 #@require_POST
